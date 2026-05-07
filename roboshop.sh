7375aa27ba4c543302d7b99d39d6ce81424b8372
Nagaraz C <raajdevops2026@gmail.com>
2026-05-07 15:29:47 +0530

    jenkins

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-SG_ID="sg-0de6e4ee2f538fad4"
+SG_ID="sg-0de6e4ee2f538fad4" # replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z069324844848A87Z3QQ"
+DOMAIN_NAME="daws88s.online"
 
 for instance in $@
 do
@@ -12,4 +14,50 @@ do
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text )
+
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
+    fi
+
+    echo "IP Address: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+
+    echo "record updated for $instance"
+
 done

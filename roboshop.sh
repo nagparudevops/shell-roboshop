e5aeab31e285adb7078a718b930badee81d41068
Nagaraz C <raajdevops2026@gmail.com>
2026-05-07 13:39:39 +0530

    jenkins

---
+++ roboshop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SG_ID="sg-0de6e4ee2f538fad4"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+done
